8425cb9db18caeb4ea5f5c4fce1ddb382a4bab71
Jose Miguel <jmn8718@gmail.com>
2016-03-31 23:56:30 +0200

    added cookie parser

--- w3/basic-auth/app.js
+++ w3/basic-auth/app.js
@@ -1,37 +1,52 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
+app.use(logger('dev'));
+app.use(cookieParser('12345-67890-09876-54321')); //secret key
 
 function auth(req, res, next){
   console.log(req.headers);
 
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('You are not authenticated, not auth provided');
-    err.status = 401;
-    next(err);
-    return;
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+      var err = new Error('You are not authenticated, not auth provided');
+      err.status = 401;
+      next(err);
+      return;
+    }
+    //we have auth headers
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    //Basic YWRtaW46cGFzc3dvcmQ=
+    if(user=='admin' && pass=='password'){
+      res.cookie('user','admin',{signed:true});
+      next() //authorized
+    } else {
+      var err = new Error('You are not authenticated, wrong params');
+      err.status = 401;
+      next(err);
+    }
+  } else { //we have a cookie
+    if(req.signedCookies.user === 'admin'){
+      console.log(req.signedCookies.user);
+      next()
+    } else {
+      var err = new Error('You are not authenticated, wrong cookie');
+      err.status = 401;
+      next(err);
+    }
   }
-  //we have auth headers
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  //Basic YWRtaW46cGFzc3dvcmQ=
-  if(user=='admin' && pass=='password'){
-    next() //authorized
-  } else {
-    var err = new Error('You are not authenticated, wrong params');
-    err.status = 401;
-    next(err);
-  }
+
 }
 
 app.use(auth)
 
-app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
